15a11b6132390cd7dcaf90d591602c045bf4025d
ASIA <Ahmedsayed.cs2019@gmail.com>
2021-04-04 15:37:42 +0200

    p1

---
+++ app/src/main/java/tableNames/user/Users.java
@@ -0,0 +1,148 @@
+package tableNames.user;
+
+public class Users {
+
+    private static final String
+            table_name = "users",
+            table_name_under_create = "users"
+            , userName="userName",fname="fname",fullName="fullName", lname="lname", gender="gender"
+            , profile_img="profile_img",user_original_img_url="user_original_img_url" ,user_thumb_img_url="user_thumb_img_url",license="license", thumb_image_url ="user_thumb_img_url",user_license="user_license"
+
+            , email="email", phone="phone"
+
+            , smoker="smoker" ,trusted ="trusted"
+            , password="password"
+            , license_image_url="license_image_url"
+            ,original ="original.jpg" ,thumb ="thumb.jpg"
+
+            , points="points" ,online="online"
+
+            , evaluation="evaluation"
+
+            ,is_created="is_created" ,permision="permision"
+            ,num_of_trips_completed="num_of_trips_completed"
+
+//            ,with="with"
+//            ,owner="owner"
+            ;
+
+//    users         //table name
+//      Y6nq3ddpIVbvVLVHankExfL6OkE3                // user id
+//          fname:"Ahmed"
+//          lname:"lname"
+//          email:"a@gmail.com"
+//          password:"123"
+//          gender:"male"
+//          image_url:"sdfjdsfi"
+//          licrndr_id:"lname"
+//          licrndr_img_url:"lname"
+//          points_1:"lname"
+//          points_2:"lname"
+//          evaluation:"lname"
+
+
+    public static String getTable_name() {
+        return table_name;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getFname() {
+        return fname;
+    }
+
+    public static String getLname() {
+        return lname;
+    }
+
+    public static String getGender() {
+        return gender;
+    }
+
+    public static String getProfile_img() {
+        return profile_img;
+    }
+
+    public static String getUser_original_img_url() {
+        return user_original_img_url;
+    }
+
+    public static String getThumb_image_url() {
+        return thumb_image_url;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPhone() {
+        return phone;
+    }
+
+    public static String getSmoker() {
+        return smoker;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static String getLicense_image_url() {
+        return license_image_url;
+    }
+
+    public static String getPoints() {
+        return points;
+    }
+
+    public static String getOnline() {
+        return online;
+    }
+
+    public static String getEvaluation() {
+        return evaluation;
+    }
+
+    public static String getIs_created() {
+        return is_created;
+    }
+
+    public static String getPermission() {
+        return permision;
+    }
+
+    public static String getNum_of_trips_completed() {
+        return num_of_trips_completed;
+    }
+
+    public static String getTrusted() {
+        return trusted;
+    }
+
+    public static String getUser_license() {
+        return user_license;
+    }
+
+    public static String getLicense() {
+        return license;
+    }
+
+    public static String getUser_thumb_img_url() {
+        return user_thumb_img_url;
+    }
+
+    public static String getFullName() {
+        return fullName;
+    }
+
+    public static String getOriginal() {
+        return original;
+    }
+
+    public static String getThumb() {
+        return thumb;
+    }
+}
+//صيني 550
